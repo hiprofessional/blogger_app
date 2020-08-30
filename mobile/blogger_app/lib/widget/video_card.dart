463b2b25b65cf71ea816aad3e0f78be19fb3e919
Timur <hi.pro.timur@gmail.com>
2020-08-30 15:57:20 +0300

    remove commented code

--- mobile/blogger_app/lib/widget/video_card.dart
+++ mobile/blogger_app/lib/widget/video_card.dart
@@ -120,10 +120,6 @@ class CustomTriangleClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     final path = Path();
-//    path.lineTo(size.width, 0);
-//    path.lineTo(size.width, size.height);
-//    path.lineTo(0, 0);
-
     path.lineTo(size.width, size.height / 2);
     path.lineTo(0, size.height);
     path.lineTo(0, 0);
